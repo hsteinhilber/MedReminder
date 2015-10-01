7effd3abbfea3939bf51fe247771414098fa0ed4
Harry Steinhilber <harry@randomcodepatterns.com>
2015-09-30 20:24:18 -0400

    renamed MainPage to MedicationListPage

--- Source/MedReminder/MedicationListPage.xaml.cs
+++ Source/MedReminder/MedicationListPage.xaml.cs
@@ -20,9 +20,9 @@ namespace MedReminder
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MedicationListPage : Page
     {
-        public MainPage()
+        public MedicationListPage()
         {
             this.InitializeComponent();
         }
