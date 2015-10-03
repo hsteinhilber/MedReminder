189f3cd8ecb76a4350510ec6dc42da6ec2d55119
Harry Steinhilber <harry.steinhilber@live.com>
2015-10-02 22:25:17 -0400

    select a different item in the list closes the commands flipview for any previous selection

--- Source/MedReminder/MedicationListPage.xaml.cs
+++ Source/MedReminder/MedicationListPage.xaml.cs
@@ -44,7 +44,74 @@ namespace MedReminder
 
         private void DefaultProfileButton_Click(object sender, RoutedEventArgs e)
         {
+             
+        }
 
+        private void MedicationList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            for (int index = 0; index < MedicationList.Items.Count; ++index)
+            {
+                if (index == MedicationList.SelectedIndex)
+                    continue;
+
+                var flipView = findElementInItemsControlItemAtIndex(MedicationList, index, "MedicationFlipView") as FlipView;
+                if (flipView == null)
+                    continue;
+
+                flipView.SelectedIndex = 0;
+            }
+        }
+
+        DependencyObject findElementInItemsControlItemAtIndex(ItemsControl itemsControl,
+                                                                int itemOfIndexToFind,
+                                                                string nameOfControlToFind)
+        {
+            if (itemOfIndexToFind >= itemsControl.Items.Count) return null;
+
+            DependencyObject depObj = null;
+            object o = itemsControl.Items[itemOfIndexToFind];
+            if (o != null)
+            {
+                var item = itemsControl.ContainerFromItem(o);
+                if (item != null)
+                {
+                    //GridViewItem it = item as GridViewItem;
+                    //var i = it.FindName(nameOfControlToFind);
+                    depObj = getVisualTreeChild(item, nameOfControlToFind);
+                    return depObj;
+                }
+            }
+            return null;
+        }
+
+        DependencyObject getVisualTreeChild(DependencyObject obj, String name)
+        {
+            DependencyObject dependencyObject = null;
+            int childrenCount = VisualTreeHelper.GetChildrenCount(obj);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var oChild = VisualTreeHelper.GetChild(obj, i);
+                var childElement = oChild as FrameworkElement;
+                if (childElement != null)
+                {
+                    //Code to take care of Paragraph/Run
+                    if (childElement is RichTextBlock || childElement is TextBlock)
+                    {
+                        dependencyObject = childElement.FindName(name) as DependencyObject;
+                        if (dependencyObject != null)
+                            return dependencyObject;
+                    }
+
+                    if (childElement.Name == name)
+                    {
+                        return childElement;
+                    }
+                }
+                dependencyObject = getVisualTreeChild(oChild, name);
+                if (dependencyObject != null)
+                    return dependencyObject;
+            }
+            return dependencyObject;
         }
     }
 }
